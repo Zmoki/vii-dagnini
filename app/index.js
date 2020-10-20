36710d692f3b0a324e370573e5b443f59b4cad9b
Zarema <zarema.khalilova@gmail.com>
2020-10-21 00:10:52 +0300

    Use fade instead of loop to changing between loop and move

--- app/index.js
+++ app/index.js
@@ -1,18 +1,17 @@
 const objects = document.querySelectorAll('.object')
 
 for (let object of objects) {
-  const loop = object.querySelector('.object__loop')
-  const move = object.querySelector('.object__move')
-
-  const moveStartsFrom = Number.parseFloat(loop.dataset.moveStartsFrom)
-  const moveEndsTo = Number.parseFloat(loop.dataset.moveEndsTo)
+  const loop = object.querySelector('.object__video_type_loop')
+  const move = object.querySelector('.object__video_type_move')
 
   const startMove = () => {
     loop.pause()
-    loop.hidden = true
+    loop.classList.toggle('object__video_hidden')
+    move.classList.toggle('object__video_hidden')
 
-    move.hidden = false
-    move.play()
+    move.addEventListener('transitionend', () => {
+      move.play()
+    }, {once: true})
 
     move.addEventListener('ended', () => {
       endMove()
@@ -20,39 +19,15 @@ for (let object of objects) {
   }
 
   const endMove = () => {
-    move.hidden = true
+    loop.classList.toggle('object__video_hidden')
+    move.classList.toggle('object__video_hidden')
 
-    loop.hidden = false
-    loop.addEventListener('canplay', () => {
-      loop.currentTime = moveEndsTo
+    loop.addEventListener('transitionend', () => {
+      loop.play()
     }, {once: true})
-    loop.play()
   }
 
   loop.addEventListener('click', () => {
-    loop.playbackRate = 3.0
-    const timeUpdateListenerThatStartMove = () => {
-      if (loop.currentTime >= moveStartsFrom) {
-        loop.playbackRate = 1.0
-        startMove()
-        loop.removeEventListener('timeupdate', timeUpdateListenerThatStartMove)
-      }
-    }
-    const timeUpdateListenerThatWaitTime = () => {
-      if (loop.currentTime < moveStartsFrom) {
-        loop.removeEventListener('timeupdate', timeUpdateListenerThatWaitTime)
-        loop.addEventListener("timeupdate", timeUpdateListenerThatStartMove);
-      }
-    }
-
-    if (loop.currentTime < moveStartsFrom) {
-      console.log('lower')
-
-      loop.addEventListener("timeupdate", timeUpdateListenerThatStartMove);
-    } else {
-      console.log('upper')
-
-      loop.addEventListener('timeupdate', timeUpdateListenerThatWaitTime)
-    }
+    startMove()
   })
 }
